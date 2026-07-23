2a6ed4a7b93c57401b01e21c173afa42c3e6cd4a
Malcolm Mohr <mohr6@wisc.edu>
2026-07-23 18:38:40 -0500

    fixed ALU bugs

--- CPUproj.srcs/sources_1/new/ALU.v
+++ CPUproj.srcs/sources_1/new/ALU.v
@@ -21,43 +21,40 @@
 
 
 module ALU(
-    input wire clk,
     input wire [15:0] a,
     input wire [15:0] b,
     input wire [1:0] op,
-    input wire flag_we, // Flag reg write enable
-    
+
     output reg [15:0] y,
-    
+
     // flags
     output reg zero_flag,
     output reg neg_flag,
     output reg carry_flag,
     output reg odd_flag
     );
-    
+
     reg [16:0] arith;
-    
+
     localparam OP_ADD = 2'b00;
     localparam OP_SUB = 2'b01;
     localparam OP_AND = 2'b10;
     localparam OP_OR  = 2'b11;
-    
-    always @(posedge clk) begin
+
+    always @(*) begin
         case(op)
-            OP_ADD: arith = a + b;
-            OP_SUB: arith = a - b;
-            OP_AND: arith = a & b;
-            OP_OR:  arith = a | b;
+            OP_ADD:  arith = {1'b0, a} + {1'b0, b};
+            OP_SUB:  arith = {1'b0, a} - {1'b0, b};
+            OP_AND:  arith = {1'b0, a & b};
+            OP_OR:   arith = {1'b0, a | b};
+            default: arith = 17'b0;
         endcase
-        
+
         y = arith[15:0];
-        
-        if (flag_we) begin
-            zero_flag <= ~(|arith);
-            neg_flag <= arith[15];
-            carry_flag <= arith[16];
-            odd_flag <= arith[0];
-        end
+
+        zero_flag  = (y == 16'b0);
+        neg_flag   = y[15];
+        carry_flag = (op == OP_ADD || op == OP_SUB) ? arith[16] : 1'b0;
+        odd_flag   = y[0];
     end
 endmodule
