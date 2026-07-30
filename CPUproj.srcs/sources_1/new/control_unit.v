93b4187e4dc286a6e85a0ff7d0a1ad2f5352e929
Malcolm Mohr <mohr6@wisc.edu>
2026-07-30 16:41:09 -0500

    Made sequencer and started on CU

---
+++ CPUproj.srcs/sources_1/new/control_unit.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Malcolm Mohr
+// 
+// Create Date: 07/30/2026 04:32:02 PM
+// Design Name: 16-bit CPU
+// Module Name: control_unit
+// Project Name: 16-bit CPU
+// Target Devices: Spartan-7 XC7S25 (Arty S7-25)
+// Tool Versions: 2025.2
+// Description: controls flow of data throughout the memory and datapath
+// 
+// Dependencies: sequencer.v
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Revision 0.02 - Started control unit
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control_unit(
+        
+    );
+endmodule
---
+++ CPUproj.srcs/sources_1/new/sequencer.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Malcolm Mohr
+// 
+// Create Date: 07/30/2026 04:35:42 PM
+// Design Name: 16-bit CPU
+// Module Name: sequencer
+// Project Name: 16-bit CPU
+// Target Devices: Spartan-7 XC7S25 (Arty S7-25)
+// Tool Versions: 2025.2
+// Description: Keeps track of the current cycle in a instruction
+// 
+// Dependencies: None
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Revision 0.02 - Made sequencer
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sequencer(
+        input wire clk,
+        input wire reset,
+        output reg [2:0] cycle_data
+    );
+    
+    always @(posedge clk) begin
+        if (reset) begin
+            cycle_data <= 3'b0;
+        end
+        else begin
+            cycle_data <= cycle_data + 1;
+        end
+    end
+    
+endmodule
